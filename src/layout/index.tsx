fb6925b0fa23e94e013d9d374e099bc552a0dcb5
PJL <1594000695@qq.com>
2024-09-29 21:35:20 +0800

    feat:头部，滚动区域

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,24 +1,46 @@
 
 import { HomeOutlined, MenuFoldOutlined, MenuUnfoldOutlined, SettingOutlined, UserOutlined } from "@ant-design/icons"
 import { Button, Layout, Menu, theme } from "antd"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Link, Outlet, useLocation } from "react-router-dom"
+import cn from 'classnames'
 const { Header, Sider, Content } = Layout
 export default function DashboardLayout() {
   const [collapsed, setCollapsed] = useState(false)
+  const conRef = useRef(null)
+  const [scrolled, setScrolled] = useState(false)
   const location = useLocation()
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  useEffect(() => {
+    //当container容器滚动100px时，设置scrolled为true,否则为false
+    const handleScroll = () => {
+      if (conRef.current.scrollTop > 100) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+    conRef.current.addEventListener('scroll', handleScroll)
+  }, [])
   return (
     <Layout style={{ height: '100%' }}>
       <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="demo-logo-vertical" style={{
           height: 32,
-          margin: 16,
+          margin: 4,
+          fontSize: 20,
+          lineHeight: '32px',
+          color: '#fff',
+          textAlign: 'center',
+          letterSpacing: 3,
           background: `rgba(255, 255, 255, .2)`,
           borderRadius: borderRadiusLG,
-        }} />
+        }}>
+          {collapsed ? 'PJL' : 'PEIJUNLEI'}
+        </div>
         <Menu
           theme="dark"
           mode="inline"
@@ -43,7 +65,9 @@ export default function DashboardLayout() {
         />
       </Sider>
       <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>
+        <Header style={{ padding: 0, background: colorBgContainer }} className={cn({
+          scrolled,
+        })}>
           <Button
             type="text"
             icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
@@ -55,7 +79,7 @@ export default function DashboardLayout() {
             }}
           />
         </Header>
-        <Content>
+        <Content className="container" ref={conRef}>
           <Outlet />
         </Content>
       </Layout>
